5ffbae2d7ea9ad8d2702ad838a5aa1d2d6f2b33d
Melanholik <Forliho@gmail.com>
2023-05-29 17:31:57 +0300

    Do chapter3 lesson21

--- src/main/java/by/melanholik/springcourse/controllers/FirstController.java
+++ src/main/java/by/melanholik/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package by.melanholik.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -8,14 +9,38 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(@RequestParam("name") String name,
-                            @RequestParam(value = "surname", required = false) String surname){
-        System.out.println("Hello " + name + " " + surname);
+    public String helloPage(@RequestParam(value = "a") Integer a,
+                            @RequestParam(value = "b") Integer b,
+                            @RequestParam(value = "action") String action,
+                            Model model) {
+        System.out.println(a + " " + b + " " + action);
+        int result;
+        String markAction;
+        switch (action) {
+            case "multiplication" -> {
+                result = a * b;
+                markAction = "*";
+            }
+            case "addition" -> {
+                result = a + b;
+                markAction = "+";
+            }
+            case "subtraction" -> {
+                result = a - b;
+                markAction = "-";
+            }
+            case "division" -> {
+                result = a / b;
+                markAction = "/";
+            }
+            default -> throw new IllegalStateException("Unexpected value: " + action);
+        }
+        model.addAttribute("calcMessage", a + " " + markAction + " " + b + " = " + result);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodByePage(){
+    public String goodByePage() {
         return "first/goodbye";
     }
 }
