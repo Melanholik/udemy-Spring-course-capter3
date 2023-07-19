fbbb9bea4e99e0877f650c4c4d0453e600b48645
Melanholik <Forliho@gmail.com>
2023-07-19 12:23:43 +0300

    Do lesson42 DB Validator

--- src/main/java/by/melanholik/springcourse/application/dbPeople/dao/PersonDAO.java
+++ src/main/java/by/melanholik/springcourse/application/dbPeople/dao/PersonDAO.java
@@ -12,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class PersonDAO {
@@ -26,6 +27,11 @@ public class PersonDAO {
 
     public List<Person> index() {
         return jdbcTemplate.query("SELECT * FROM person", new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public Optional<Person> show(String email){
+        return jdbcTemplate.query("SELECT * FROM person WHERE email = ?", new Object[]{email},
+                new BeanPropertyRowMapper<>(Person.class)).stream().findAny();
     }
 
     public Person show(int id) {
